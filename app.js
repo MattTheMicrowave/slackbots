6cca85654403ba75d4d034817dda847c9dc1699e
MattTheMicrowave <mwlluis@hotmail.com>
2016-06-16 15:32:55 -0500

    ciao

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.listen(port, function () {
   console.log('Listening on port ' + port);
 });
 
-app.post('/hello', function (req, res, next) {
+app.post('/ciao', function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
     text : 'Hello ' + userName + ', welcome to Devdactic Slack channel! I\'ll be your guide.'
